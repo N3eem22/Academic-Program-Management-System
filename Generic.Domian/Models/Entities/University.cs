c45c2b3760e555bae4101471f344e94d3bfc4692
N3eem22 <ahmednaeem200393@gmail.com>
2024-02-13 19:28:20 +0200

    First Commit for Lockups

---
+++ Generic.Domian/Models/Entities/University.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic.Domian.Models.Entities
+{
+    [Table("University")]
+    public class University : BaseEntity
+    {
+        [MaxLength(70)]
+        public string Name { get; set; }
+        [MaxLength(250)]
+        public string Location { get; set; }
+    }
+}
--- Generic.Domian/obj/Release/net7.0/Generic.Domian.AssemblyInfo.cs
+++ Generic.Domian/obj/Release/net7.0/Generic.Domian.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generic.Domian")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fa48416510e9962dbe5c0683b741c9aded3406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generic.Domian")]
