5072183c2e70eedab35a354d4fed449772cf6a1b
N3eem22 <ahmednaeem200393@gmail.com>
2024-06-07 01:52:31 +0300

    logs

--- Talabat.Core/Specifications/LockUps spec/ProgramSpec.cs
+++ Talabat.Core/Specifications/LockUps spec/ProgramSpec.cs
@@ -1,20 +1,17 @@
 ﻿using Grad.Core.Entities.Graduation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Talabat.Core.Specifications;
 
 namespace Grad.Core.Specifications.LockUps_spec
 {
     public class ProgramSpec : BaseSpecifications<Programs>
     {
-        public ProgramSpec(int? FacultyId)
-          : base(c => (!FacultyId.HasValue || c.FacultyId == FacultyId.Value && c.IsDeleted == false))
-        
-            {
-              Includes.Add(P => P.Faculty);      
-            }
+        public ProgramSpec(int facultyId, string? searchValue)
+            : base(c =>
+                (c.FacultyId == facultyId) &&
+                (string.IsNullOrEmpty(searchValue) || c.ProgramNameInArabic.Contains(searchValue)) &&
+                c.IsDeleted == false)
+        {
+            Includes.Add(p => p.Faculty);
+        }
     }
 }
